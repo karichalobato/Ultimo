72a3ef6f92cc3e20a6ca8013e90eb1e0ffcb282b
Karicha Lobato <00002517@uca.edu.sv>
2018-06-18 20:20:53 -0600

    Terminado

---
+++ cortoLaboPoo/src/GUI/Ventana.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import base.Filtro;
+import base.FiltroDao;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+import javafx.scene.control.ComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
+ */
+
+public class Ventana extends JPanel{
+    
+    public int WIDTH = 900, widthTF = 120, widthB = 100;
+    public int HEIGHT = 500, heightTF = 30, heightB = 30;
+    public JTextField nombre, codigo, precio, cantidad;
+    public JLabel lblnombre,lblcodigo,lblprecio,lblcantidad;
+    public JButton actualizar, eliminar, limpiar,insertar,buscar;
+    public ComboBox tipo;
+    private FiltroDao filtro;
+    Scanner read = new Scanner(System.in);
+    
+    public Ventana(){
+        this.filtro = new FiltroDao();
+        
+        this.lblnombre = new JLabel("nombre");
+        lblnombre.setBounds(25,25,70,30);
+        
+        this.nombre = new JTextField();
+        nombre.setBounds(new Rectangle(90,25,widthTF, heightTF));
+        
+        this.lblcodigo = new JLabel("codigo");
+        lblcodigo.setBounds(25,55,70,30);
+        
+        this.codigo = new JTextField();
+        codigo.setBounds(new Rectangle(90,55,widthTF, heightTF));
+        
+        this.lblprecio = new JLabel("precio");
+        lblprecio.setBounds(25,85,70,30);
+        
+        this.precio = new JTextField();
+        precio.setBounds(90,85,widthTF, heightTF);
+        
+        this.lblcantidad = new JLabel("Cantidad");
+        lblcantidad.setBounds(25,115,70,30);
+        
+        this.cantidad = new JTextField();
+        cantidad.setBounds(90,115,widthTF, heightTF);
+        
+        this.actualizar = new JButton("actualizar");
+        actualizar.setBounds(250,115,widthTF, heightTF);
+        
+        this.eliminar = new JButton("eliminar");
+        eliminar.setBounds(250+widthTF,115,widthTF, heightTF);
+        
+        this.limpiar = new JButton("limpiar");
+        limpiar.setBounds(250+widthTF*2,115,widthTF, heightTF);
+        
+        this.insertar = new JButton("insertar");
+        insertar.setBounds(250+widthTF*3,115,widthTF, heightTF);
+        
+        this.buscar = new JButton("buscar");
+        buscar.setBounds(250,25,widthTF, heightTF);
+        
+        buscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Filtro f = filtro.read(codigo.getText());
+                
+                System.out.println(f.getNombre() + " - "+f.getCodigo());
+            }
+        });
+        
+        actualizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int cantidadl = Integer.parseInt(cantidad.getText());
+                int preciol = Integer.parseInt(precio.getText());
+                //int cantidad, int precio, boolean disponibilidad
+                filtro.update(new Filtro(codigo.getText(),cantidadl,preciol,true));
+                
+            }
+        });
+        
+        add(nombre);
+        add(lblnombre);
+        add(codigo);
+        add(lblcodigo);
+        add(precio);
+        add(lblprecio);
+        add(cantidad);
+        add(lblcantidad);
+        add(actualizar);
+        add(eliminar);
+        add(limpiar);
+        add(insertar);
+        add(buscar);
+        setLayout(null);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        
+    }
+    
+}
---
+++ cortoLaboPoo/src/base/FiltroDao.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
+ */
+public class FiltroDao implements Metodos <Filtro>{
+    private static final String SQL_INSERT = "INSERT INTO productos (id,nombre,codigo,tipo,cantidad,precio,disponibilidad) VALUES (?,?,?,?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE productos SET  cantidad = ?, precio = ? , disponibilidad = ? WHERE codigo = ?";
+    private static final String SQL_DELETE = " DELETE FROM productos WHERE codigo = ?";
+    private static final String SQL_READ = "SELECT * FROM productos WHERE codigo = ?";
+    private static final String SQL_READALL = "SELECT * FROM productos";
+
+    public static final Conexion con = Conexion.conectar();
+
+    @Override
+    public boolean create(Filtro g) {
+
+        PreparedStatement ps;
+
+        try {
+            ps = con.getCnx().prepareStatement(SQL_INSERT);
+            ps.setInt(1, g.getId());
+            ps.setString(2, g.getNombre());
+            ps.setString(3, g.getCodigo());
+            ps.setString(4, g.getTipo());
+            ps.setInt(5, g.getCantidad());
+            ps.setInt(6, g.getPrecio());
+            ps.setBoolean(7, g.isDisponibilidad());
+            if (ps.executeUpdate() > 0) {
+                return true;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+
+        } finally {
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Object key) {
+        try {
+            PreparedStatement ps;
+            
+            ps = con.getCnx ().prepareStatement(SQL_DELETE);
+            ps.setString(1, key.toString());
+            if(ps.executeUpdate()>0){
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(Filtro c) {
+        PreparedStatement ps;
+        try {
+            
+            //id,nombre,codigo,tipo,cantidad,precio,disponibilidad
+            System.out.println(c.getCodigo());
+            ps = con.getCnx().prepareStatement(SQL_UPDATE);
+            
+            ps.setInt(1, c.getCantidad());
+            ps.setInt(2, c.getPrecio());
+            ps.setBoolean(3, c.isDisponibilidad());
+            if(ps.executeUpdate()>0){
+                return true;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            con.cerrarConexion();
+        }
+        return false;
+    }
+
+    @Override
+    public Filtro read(Object key) {
+        Filtro f = null;
+        PreparedStatement ps;
+        ResultSet rs;
+        try {
+
+            ps = con.getCnx().prepareStatement(SQL_READ);
+            ps.setString(1, key.toString());
+
+            rs = ps.executeQuery();
+            //id,nombre,codigo,tipo,cantidad,precio,disponibilidad
+
+            while (rs.next()) {
+                f = new Filtro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(4), rs.getInt(5) ,rs.getBoolean(7));
+                
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            con.cerrarConexion();
+        }
+        return f;
+    }
+
+    @Override
+    public ArrayList<Filtro> readAll() {
+        ArrayList<Filtro> all = new ArrayList();
+        Statement s;
+        ResultSet rs;
+        try {
+
+            s = con.getCnx().prepareStatement(SQL_READALL);
+            rs = s.executeQuery(SQL_READALL);
+            
+            while(rs.next()){
+                all.add(new Filtro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(4), rs.getInt(5) ,rs.getBoolean(7)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FiltroDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return all;
+    }
+}
