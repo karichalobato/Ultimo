fef9ccd05055620b610a9fb3f36cca2792aeed26
Karicha Lobato <00002517@uca.edu.sv>
2018-06-18 19:53:13 -0600

    Main

---
+++ ConexionBD_POO/src/vista/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author Karicha Valesska Romero Lobato <00002517@uca.edu.sv>
+ */
+public class Main {
+    public static void main(String[] args){
+       Consulta consulta = new Consulta ();
+       consulta.setVisible(true);  
+         
+     } 
+}
